72996e1cb2e86b38576f751a05ce9b8e951eb2aa
Daniel <daniel.shevchenko03@gmail.com>
2026-08-13 11:59:39 +0300

    add SensorEvent model and init migration

--- backend/events/models.py
+++ backend/events/models.py
@@ -1,3 +1,9 @@
 from django.db import models
 
 # Create your models here.
+class SensorEvent(models.Model):
+    response_time = models.DateTimeField(auto_now_add=True)
+    distance_value = models.FloatField()
+
+    def __str__(self):
+        return f"Get new response: at {self.response_time}, distance {self.distance_value}"
