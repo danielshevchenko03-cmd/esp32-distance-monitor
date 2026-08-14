51be5c430e4e9de5268daae0586411d6329cf2c0
Daniel <daniel.shevchenko03@gmail.com>
2026-08-14 15:23:49 +0300

    add /stats command, improve /help formatting

--- bot/bot.py
+++ bot/bot.py
@@ -22,13 +22,18 @@ async def start(update: Update, context: ContextTypes.DEFAULT_TYPE):
     await update.message.reply_text("Hello!")
 
 async def help_command(update: Update, context: ContextTypes.DEFAULT_TYPE):
-    await update.message.reply_text("*/start* - почати роботу з ботом\n*/help* - список команд\n*/get_data* - отримати CSV з даними датчика", parse_mode="Markdown")
+    text = """"*/start* - почати роботу з ботом
+    */help* - список команд
+    */get_data* - отримати CSV з даними сенсора
+    */stats* - статистика сенсора"""
+    await update.message.reply_text(text, parse_mode="Markdown")
 
 async def post_init(application):
     commands = [
         BotCommand("start", "Почати роботу з ботом"),
         BotCommand("help", "Список команд"),
-        BotCommand("get_data", "Отримати CSV з даними датчика"),
+        BotCommand("get_data", "Отримати CSV з даними сенсора"),
+        BotCommand("stats", "Статистика сенсора"),
     ]
     await application.bot.set_my_commands(commands)
 
@@ -58,6 +63,28 @@ async def get_data(update: Update, context: ContextTypes.DEFAULT_TYPE):
     except Exception as error:
         await update.message.reply_text("Виникла помилка при формуванні звіту")
 
+async def stats(update: Update, context: ContextTypes.DEFAULT_TYPE):
+    try:
+        response = requests.get(API_URL)
+        response.raise_for_status()
+
+        data = response.json()
+        total_count = len(data)
+
+        if len(data) != 0:
+            last_event = data[-1]
+            message = f"Всього подій: {total_count}\nОстання подія: {last_event['time']}, {last_event['distance']} мм"
+            await update.message.reply_text(message)
+
+        else:
+            await update.message.reply_text("Подій не виявлено")
+
+    except requests.RequestException:
+        await update.message.reply_text("⚠️ Сервер бекенду тимчасово недоступний ⚠️")
+
+    except Exception as error:
+        await update.message.reply_text("Виникла помилка при формуванні звіту")    
+
 
 if __name__ == '__main__':
     app = ApplicationBuilder().token(TOKEN).post_init(post_init).build()
@@ -65,6 +92,7 @@ if __name__ == '__main__':
     app.add_handler(CommandHandler("start", start))
     app.add_handler(CommandHandler("help", help_command))
     app.add_handler(CommandHandler("get_data", get_data))
+    app.add_handler(CommandHandler("stats", stats))
 
-    print("🤖 Бот запущен!")
+    print("🤖 Bot launched!")
     app.run_polling()
